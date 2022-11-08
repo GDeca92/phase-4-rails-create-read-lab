dcc795b2ed30983aba1887a6199e649dcf270ea8
Gian Carlo De Castro <Giancld92@gmail.com>
2022-11-08 06:23:49 -0600

    commit tree f5a00fcc10004a3b7a2375f880e20f3287eecdf5

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,25 @@
+class PlantsController < ApplicationController
+    def index
+        render json: Plant.all, except: [:created_at, :updated_at]
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant, except: [:created_at, :updated_at]
+        else
+            render json: {error: "Plant not found"}
+        end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created, except: [:created_at, :updated_at]
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
